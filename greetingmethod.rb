4ae95700aaea5aac64baecfaf6588bcb183454a0
Grace Park <parkgra3@gmail.com>
2018-07-24 14:07:18 +0000

    Done.

---
+++ greetingmethod.rb
@@ -0,0 +1,2 @@
+greeting("What's up", "Kim", "Kanye", "morning")
+#> "What's up Kim, I'm Kanye, how's your morning?"
---
+++ hellokwk.rb
@@ -0,0 +1,4 @@
+def say_hello
+  your_name = "Grace"
+  puts "Hello #{your_name}."
+end
